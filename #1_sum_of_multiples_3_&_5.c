cb5fa86dcbb9871c30e59f4d26470f5c34a73765
sam-abs <55846301+sam-abs@users.noreply.github.com>
2019-10-01 20:49:05 +0530

    Update #1_sum_of_multiples_3_&_5.c

--- #1_sum_of_multiples_3_&_5.c
+++ #1_sum_of_multiples_3_&_5.c
@@ -3,14 +3,14 @@ int main()
 {
     long unsigned int i,sum=0;
     clrscr();
-    for(i=0;i<1000;i++)
+    for(i=0;i<1000;i++)//This loop traverses among the first 1000 integers
     {
-        if((i%5==0)||(i%3==0))
+        if((i%5==0)||(i%3==0))//Checks whether it is a multiple of 5 and 3
         {
-            sum=sum+i;
+            sum=sum+i;//Finds the sum of such numbers
         }
     }
-    printf("%d\n",sum);
+    printf("%d\n",sum);//Prints the sum of multiples
     getchar();
     return 0;
 }
